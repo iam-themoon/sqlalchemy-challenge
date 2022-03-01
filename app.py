7cb308007b90042d6fe12ed048bdce961541d9ed
Joshua M <joshua.moon10@gmail.com>
2022-02-28 20:31:21 -0500

    Commit for completed app. No bonus this time

--- app.py
+++ app.py
@@ -1,29 +1,33 @@
 # imports and dependencies
-import numpy as numpy
+import numpy as np
 import sqlalchemy 
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, fuc, distinct
-from flask import Flask, jsonify
+from sqlalchemy import create_engine, func, distinct
+from flask import Flask, jsonify, render_template, request
+import datetime as dt
 
 #################################################
 # Database Setup
 #################################################
+
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
-
-# reflect an existing database into a new model
+# Reflect an existing database into a new model
 Base = automap_base()
-# reflect the tables
+
+# Reflect the tables
 Base.prepare(engine, reflect = True)
 
 # Save references to each table
-measurement = Base.classes.measurement
-station = base.classes.station
+Measurement = Base.classes.measurement
+
+Station = Base.classes.station
 
 #################################################
 # Flask Setup
 #################################################
+
 app = Flask(__name__)
 
 #################################################
@@ -32,89 +36,130 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    """List all available api routes."""
-    return (
-        f"Welcome to the homepage! <br/>"
-        f"Available routes: <br/>"
+     return (
+        f"<h1> Welcome to Joshua Moon's HW 10 Assignment!</h1>"
+        f"<h2> Here are the available routes: </h2><br/>"
+        f"<h3> (0) Precipitation Data: <a href=\"/api/v1.0/precipitation\">/api/v1.0/precipitation<a></h3><br/>"
+        f"<h3> (0) Station Data: <a href=\"/api/v1.0/stations\">/api/v1.0/stations<a></h3><br/>"
+        f"<h3> (0) Temp Observation (TOBS) Data:  <a href=\"/api/v1.0/tobs\">/api/v1.0/tobs<a></h3><br/>"
+        f"<h3> (0) Min, Average, & Max Temperatures for Date Range (Pre-filled Jan - Dec 2014): <a href=\"/api/v1.0/2014-01-01/2014-12-31\">/api/v1.0/2014-01-01/2014-12-31<a></h3><br/>"
     )
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    # Create our session (link) from Python to the DB
+    # Create session to link to database
     session = Session(engine)
 
-    """Return a list of all daily precipitation totals for the last year"""
-    # Query and summarize daily precipitation across all stations for the last year of available data 
-    
-    start_date = '2016-08-23'
-    sel = [measurement.date, func.sum(measurement.prcp)]
-    precipitation = session.query(*sel).\
-        filter(measurement.date >= start_date).\
-        group_by(measurement.date).\
-        order_by(measurement.date).all()
-
+    # Query and summarize precipitation across all stations     
+    sel = [Measurement.date, Measurement.prcp]
+    result = session.query(*sel).all()
     session.close()
 
-    # Create dictionary from results and jsonify it
-    prcp_dates = []
-    prcp_totals = []
+    precipitation = []
+    for date, prcp in result:
+        prcp_dict = {}
+        prcp_dict["Date"] = date
+        prcp_dict["Precipitation"] = prcp
+        precipitation.append(prcp_dict)
 
-    from date, dailytotal in precipitation:
-        prcp_dates.append(date)
-        prcp_totals.append(dailytotal)
+    return jsonify(precipitation)
 
-    prcp_dict = dict(zip(prcp_dates, prcp_totals))
-
-    return jsonify(prcp_dict)
-        
 
 @app.route("/api/v1.0/stations")
 def stations():
-    # Create session (link) from Python to the DB
+    # Create session to link to database
     session = Session(engine)
 
-    """Return list of all weather stations in HI"""
-    # Return list of active weather stations in HI
-    sel = [measurement.station]
-    active_stations = session.query(*sel).\
-        group_by(measure.station).all()
+    # Query all stations
+    sel = [Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation]
+    result = session.query(*sel).all()
     session.close()
 
-    # Convert list of tuples into normal list
-    all_stations = lsit(np.ravel(active_stations))
-    return jsonify(all_stations)
+    stations = []
+    for station, name, lat, lng, elv in result:
+        station_dict = {}
+        station_dict["Station"] = station
+        station_dict["Name"] = name
+        station_dict["Lat"] = lat
+        station_dict["Lng"] = lng
+        station_dict["Elevation"] = elv
+        stations.append(station_dict)
+
+    return jsonify(stations)
+
 
 @app.route("/api/v1.0/tobs")
+def tobs():
+    # Create session to link to database
+    session = Session(engine)
+
+    # Find the latest date for querying
+    latest_str = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
+    latest_date = dt.datetime.strptime(latest_str, '%Y-%m-%d')
+    query_date = dt.date(latest_date.year -1, latest_date.month, latest_date.day)
+
+    # Query all tobs
+    sel = [Measurement.date, Measurement.tobs]
+    result = session.query(*sel).filter(Measurement.date >= query_date).all()
+    session.close()
+    
+    # Parse through query and append to dict then to list
+    all_tobs = []
+    for date, tobs in result:
+        tobs_dict = {}
+        tobs_dict['Date'] = date
+        tobs_dict['Tobs'] = tobs
+        all_tobs.append(tobs_dict)
+
+    return jsonify(all_tobs)
 
 
+@app.route("/api/v1.0/<start>")
+def Start(start):
+    # Create session to link to database
+    session = Session(engine)
+
+    # Query
+    result = session.query(func.min(Measurement.tobs),
+                            func.avg(Measurement.tobs),
+                            func.max(Measurement.tobs)).\
+                filter(Measurement.date >= start_date).all()    
+    session.close()
+
+    # Create dict from row data and append to a list
+    start_list = []
+    for min, avg, max in result:
+        start_dict = {}
+        start_dict["Min"] = min
+        start_dict["Average"] = avg
+        start_dict["Max"] = max
+        start_list.append(start_dict)
+
+    return jsonify(start_list)
 
 
+@app.route("/api/v1.0/<start>/<end>")
+def Start_end(start, end):
+    # Create session to link to database
+    session = Session(engine)
 
+    # Query
+    results = session.query(func.min(Measurement.tobs),
+                            func.avg(Measurement.tobs),
+                            func.max(Measurement.tobs)).\
+                filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    session.close()
 
+    # Create dict from data and append to list
+    start_end_list = []
+    for min, avg, max in results:
+            start_end_dict = {}
+            start_end_dict["Min"] = min
+            start_end_dict["Average"] = avg
+            start_end_dict["Max"] = max
+            start_end_list.append(start_end_dict)
 
-
-
-
-
-
-
-# 2. Create an app, being sure to pass __name__
-app = Flask(__name__)
-
-
-# 3. Define what to do when a user hits the index route
-@app.route("/")
-def home():
-    print("Server received request for 'Home' page...")
-    return "Welcome to my 'Home' page!"
-
-
-# 4. Define what to do when a user hits the /about route
-@app.route("/about")
-def about():
-    print("Server received request for 'About' page...")
-    return "Welcome to my 'About' page!"
-
+    return jsonify(start_end_list)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug = True)
